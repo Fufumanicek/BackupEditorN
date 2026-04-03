423476327426fa811f0b131bbe8221970b8bab07
DolezalFilip <DolezalFilip@sssvt.cz>
2026-04-03 18:30:14 +0200

    adding methods in PathSelectorWindow

--- BackupEditorN/Presentation/Windows/PathSelectorWindow.cs
+++ BackupEditorN/Presentation/Windows/PathSelectorWindow.cs
@@ -1,6 +1,35 @@
-﻿namespace BackupEditorN.Presentation.Windows;
+﻿using BackupEditorN.Presentation.Components;
+namespace BackupEditorN.Presentation.Windows;
 
 public class PathSelectorWindow
 {
+    //data
+    private List<string> _selectedPaths { get; set; }
     
+    //komponenty
+    private FileSystemPanel _fileSystemPanel { get; set; }
+    private ListPanel _labelPanel { get; set; }
+    private Button _okButton { get; set; }
+    private Button _cancelButton { get; set; }
+    
+    public List<string> ResultPaths { get; private set;}
+    
+    public PathSelectorWindow(string title, List<string> currentPaths, Application app, IWindow returnWindow)
+    {
+        
+    }
+    
+    private void OnFolderSelected(string path)
+    {
+        
+    }
+    
+    private void OkClicked()
+    {
+        
+    }
+    private void CancelClicked()
+    {
+        
+    }
 }
