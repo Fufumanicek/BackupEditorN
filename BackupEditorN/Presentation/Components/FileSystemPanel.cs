e407073b80ab7f6c40caf9baba72209377d629b3
DolezalFilip <DolezalFilip@sssvt.cz>
2026-04-03 16:13:59 +0200

    adding constructors and methods in FileSystemPanel and ListPanel

---
+++ BackupEditorN/Presentation/Components/FileSystemPanel.cs
@@ -0,0 +1,39 @@
+﻿namespace BackupEditorN.Presentation.Components;
+
+public class FileSystemPanel : BaseComponent
+{
+    public event Action<string>? folderSelected;
+    
+    public override bool Selectable => true;
+    
+    private string _currentPath { get; set; }
+    private List<string> _entries { get; set; }
+    private int _selectedIndex { get; set; }
+    private int _visibleCount { get; set; }
+    private string _title { get; set; }
+
+    public FileSystemPanel(string title = "Průzkumník", int visibleCount = 12)
+    {
+        
+    }
+    
+    private void LoadEntries()
+    {
+        
+    }
+    
+    private void NavigateTo(string path)
+    {
+        
+    }
+
+    public override void Render(bool selected)
+    {
+        
+    }
+    
+    public override void HandleKey(ConsoleKeyInfo keyInfo)
+    {
+        
+    }
+}
---
+++ BackupEditorN/Presentation/Components/ListPanel.cs
@@ -0,0 +1,39 @@
+﻿namespace BackupEditorN.Presentation.Components;
+
+public class ListPanel : BaseComponent
+{
+    public override bool Selectable => true;
+    public List<string> Items { get; set; }
+    public string? SelectedItem 
+        => Items.Count > 0
+        ? Items[Math.Min(_selectedIndex, Items.Count - 1)] : null;
+    
+    private int _selectedIndex { get; set; }
+    private int _visibleCount { get; set; }
+    private string _title { get; set; }
+
+    public ListPanel(string title, int visibleCount = 10)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddItem(string path)
+    {
+        
+    }
+    
+    public void RemoveSelected()
+    {
+        
+    }
+    
+    public override void Render(bool selected)
+    {
+        
+    }
+    
+    public override void HandleKey(ConsoleKeyInfo keyInfo)
+    {
+        
+    }
+}
