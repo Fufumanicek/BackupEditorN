8663fe5601eef9ba8cb078366ffd3db5fd10b407
DolezalFilip <DolezalFilip@sssvt.cz>
2026-04-04 14:58:59 +0200

    updated Program.cs to initialize Application with MainWindow and extended CronValidator to implement GetNextOccurrence logic for parsing and matching cron expressions

--- BackupEditorN/Program.cs
+++ BackupEditorN/Program.cs
@@ -1,10 +1,15 @@
-﻿using P3ABackupN;
+﻿using P3ABackupN.Services;
+using BackupEditorN.Services;
+using BackupEditorN.Presentation.Windows;
 namespace BackupEditorN;
 
 class Program
 {
     static void Main(string[] args)
     {  
-        Console.WriteLine("Hello, World!");
+        var configService = new ConfigurationService();
+        var app = new Application();
+        var mainWindow = new MainWindow(configService, app);
+        app.Run(mainWindow);    
     }
 }
--- BackupEditorN/Services/CronValidator.cs
+++ BackupEditorN/Services/CronValidator.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using P3ABackupN.Services;
 namespace BackupEditorN.Services;
 
 public class CronValidator
@@ -42,15 +42,38 @@ public class CronValidator
         return true;
     }
 
-    // public string? GetNextOccurence(string cronString)
-    // {
-    //     if (!IsValid(cronString)) return null;
-    //
-    //     string[] parts = cronString.Trim().Split(' ');
-    //     DateTime dt = DateTime.Now.AddMinutes(1);
-    //     dt = new DateTime(dt.Year, dt.Month, dt.Day, dt.Hour, dt.Minute, 0);
-    //
-    //     for (int i = 0; i < 60 * 24 * 366; i++ , dt = dt.AddMinutes(1))
-    //
-    // }
+     public string? GetNextOccurence(string cronString)
+    {
+         if (!IsValid(cronString)) return null;
+    
+         string[] parts = cronString.Trim().Split(' ');
+         DateTime dt = DateTime.Now.AddMinutes(1);
+         dt = new DateTime(dt.Year, dt.Month, dt.Day, dt.Hour, dt.Minute, 0);
+
+         for (int i = 0; i < 60 * 24 * 366; i++, dt = dt.AddMinutes(1))
+         {
+             if (Matches(parts[0], dt.Minute) &&
+                 Matches(parts[1], dt.Hour) &&
+                 Matches(parts[2], dt.Day) &&
+                 Matches(parts[3], dt.Month) &&
+                 Matches(parts[4], (int)dt.DayOfWeek))
+             {
+                 return dt.ToString("yyyy-MM-dd HH:mm");
+             }
+         }
+         
+         return null;
+    
+    }
+
+    private bool Matches(string part, int value)
+    {
+        if(part == "*") return true;
+        if (part.Contains("-"))
+        {
+            string[] range = part.Split('-');
+            return value >= int.Parse(range[0]) && value <= int.Parse(range[1]);
+        }
+        return int.Parse(part) == value;
+    }
 }
