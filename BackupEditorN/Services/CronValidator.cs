8c548ef09f72a62c199bd5b3acd0a7dc6aa899d2
DolezalFilip <DolezalFilip@sssvt.cz>
2026-04-04 13:30:40 +0200

    improved CronValidator's `IsValid` method to add validation logic for cron expressions

--- BackupEditorN/Services/CronValidator.cs
+++ BackupEditorN/Services/CronValidator.cs
@@ -1,14 +1,56 @@
-﻿namespace BackupEditorN.Services;
+﻿
+namespace BackupEditorN.Services;
 
 public class CronValidator
 {
-    public void IsValid(string cronString)
+    private static readonly (int Min, int Max)[] ranges =
     {
+
+        (0, 59), //minuta
+        (0, 23), //hodina
+        (1, 31), //den
+        (1, 12), //mesic
+        (1, 7)  //den v tydnu
+
+    };
+    public bool IsValid(string cronString)
+    {
+        if (string.IsNullOrWhiteSpace(cronString)) return false;
+        string[] parts = cronString.Trim().Split(' ');
         
+        if (parts.Length != 5) return false;
+        
+        for (int i = 0; i < 5; i++)
+        {
+            if (parts[i] == "*") continue;
+
+            if (parts[i].Contains("-"))
+            {
+                string[] range = parts[i].Split('-');
+                if (range.Length != 2) return false;
+                if (!int.TryParse(range[0], out int from)) return false;
+                if (!int.TryParse(range[1], out int to)) return false;
+                if (from < ranges[i].Min || to > ranges[i].Max) return false;
+            }
+
+            else
+            {
+                if (!int.TryParse(parts[i], out int value)) return false;
+            }
+        }
+        
+        return true;
     }
 
-    public string? GetNextOccurence(string cronString)
-    {
-        return null;
-    }
+    // public string? GetNextOccurence(string cronString)
+    // {
+    //     if (!IsValid(cronString)) return null;
+    //
+    //     string[] parts = cronString.Trim().Split(' ');
+    //     DateTime dt = DateTime.Now.AddMinutes(1);
+    //     dt = new DateTime(dt.Year, dt.Month, dt.Day, dt.Hour, dt.Minute, 0);
+    //
+    //     for (int i = 0; i < 60 * 24 * 366; i++ , dt = dt.AddMinutes(1))
+    //
+    // }
 }
